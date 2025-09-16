0c1c1297b5aa438669cbf4bb1f12db5c521864e7
PJJunio <paulodjunior.dev@gmail.com>
2025-09-16 15:25:42 -0300

    refac: compactando query de busca para uma melhor visualização

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,29 +7,21 @@ use App\Database\Query;
 class User
 {
     private Query $conn;
-    
+
     public function __construct()
     {
         $this->conn = new Query();
     }
 
-    public function getEmail($email)
+    public function getUserByEmail(string $email): ?array
     {
-        $result = $this->conn->count("usuario", "email = ?", [$email]);
+        $result = $this->conn->select('usuario', 'email = ?', [$email], 'email, senha, nome');
 
-        if ($result === false || empty($result)) {
-            echo "Erro ao buscar usuário ou resultado vazio";
-            exit;
-
-        }
-
-        $count = (int) $result[0]['COUNT(*)'];
-
-        if ($count > 0) {
-            echo "Usuario encontrado";
-
-        } else {
-            echo "Usuario não encontrado";
-        }
+        return $result[0] ?? null;
     }
+
+    // public function checkPassword(string $password, string $hashedPassword): bool
+    // {
+    //     return password_verify($password, $hashedPassword);
+    // }
 }
