b277ae74e2ad536fc74d30dda7ed9f3520de00ed
PJJunio <paulodjunior.dev@gmail.com>
2025-09-16 13:52:16 -0300

    feat: adicionando validação para apenas usuarios com sessão acessarem páginas especificas

--- public/view/includes/header.php
+++ public/view/includes/header.php
@@ -2,6 +2,12 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_erros', 1);
 error_reporting(E_ALL);
+
+session_start();
+
+if (empty($_SESSION)) {
+    header('Location: /login');
+}
 ?>
 
 <!doctype html>
