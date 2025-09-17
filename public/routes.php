303a5a08425450e115519df5d7ffe8255d02aae3
PJJunio <paulodjunior.dev@gmail.com>
2025-09-17 10:45:57 -0300

    feat: adicionando rotas de editar e criar produto

--- public/routes.php
+++ public/routes.php
@@ -5,6 +5,7 @@ use App\Controller\Auth\LoginController;
 use App\Controller\Auth\LogoffController;
 use App\Controller\Category\NewCategoryController;
 use App\Controller\Category\CategoryController;
+use App\Controller\EditProductController;
 use App\Controller\Error\ErrorController;
 use App\Controller\Error\NotFoundController;
 use App\Controller\ProductController;
@@ -16,6 +17,8 @@ $router = [
         '/logoff' => LogoffController::class,
         '/error' => ErrorController::class,
         '/product' => ProductController::class,
+        '/product/new' => ProductController::class,
+        '/product/edit' => EditProductController::class,
         '/category' => CategoryController::class,
         '/new_category' => NewCategoryController::class,
     ],
