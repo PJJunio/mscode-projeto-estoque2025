19e75f23c6daad500c670fa85a320d815fe99579
PJJunio <paulodjunior.dev@gmail.com>
2025-09-17 10:44:37 -0300

    refac: alterando diretorio do icone

--- public/view/includes/header.php
+++ public/view/includes/header.php
@@ -30,7 +30,7 @@ if (empty($_SESSION)) {
             <header class="d-flex flex-wrap justify-content-center align-items-center py-3">
                 <a href="/"
                     class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <img class="me-2" src="assets/images/logotipo.svg" width="53">
+                    <img class="me-2" src="/assets/images/logotipo.svg" width="53">
                     <span class="fs-4">MS Code - Estoque</span>
                 </a>
 
