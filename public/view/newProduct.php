eb4e9f7eb80503e547f9c2309ad753e0401e3b64
PJJunio <paulodjunior.dev@gmail.com>
2025-09-20 23:47:22 -0300

    refac: adaptando o codigo para que quando der erro de preencher todos os campos, os campos nao serem limpos

--- public/view/newProduct.php
+++ public/view/newProduct.php
@@ -15,26 +15,26 @@ require_once __DIR__ . '/includes/header.php';
         } ?>
         <div class="mb-3">
           <label for="nome" class="form-label">Nome:</label>
-          <input type="text" class="form-control" id="nome" name="nome">
+          <input type="text" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($data['formData']['nome'] ?? ''); ?>">
         </div>
         <div class="mb-3">
           <label for="descricao" class="form-label">Descrição:</label>
-          <textarea class="form-control" id="descricao" name="descricao" style="resize:none;" rows="5"></textarea>
+          <textarea class="form-control" id="descricao" name="descricao" style="resize:none;" rows="5"><?php echo htmlspecialchars($data['formData']['descricao'] ?? ''); ?></textarea>
         </div>
 
 
         <div class="row">
           <div class="mb-3 col-4">
             <label for="categoriaId" class="form-label">Categoria Id:</label>
-            <input type="number" class="form-control" id="categoriaId" name="categoriaId">
+            <input type="number" class="form-control" id="categoriaId" name="categoriaId" value="<?php echo htmlspecialchars($data['formData']['categoriaId'] ?? ''); ?>">
           </div>
           <div class="mb-3 col-4">
             <label for="quantidade" class="form-label">Quantidade:</label>
-            <input type="number" class="form-control" id="quantidade" name="quantidade">
+            <input type="number" class="form-control" id="quantidade" name="quantidade" value="<?php echo htmlspecialchars($data['formData']['quantidade'] ?? ''); ?>">
           </div>
           <div class="mb-3 col-4">
             <label for="valor" class="form-label">Valor:</label>
-            <input type="text" class="form-control" id="valor" name="valor">
+            <input type="text" class="form-control" id="valor" name="valor" value="<?php echo htmlspecialchars($data['formData']['valor'] ?? ''); ?>">
           </div>
         </div>
 
