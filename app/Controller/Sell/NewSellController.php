a74a97733e80434e01aa5b9eb4c692aca1c8e5d8
PJJunio <paulodjunior.dev@gmail.com>
2025-09-22 14:19:08 -0300

    feat: iniciando e testando formularios recebidos de newSell.php

--- app/Controller/Sell/NewSellController.php
+++ app/Controller/Sell/NewSellController.php
@@ -8,6 +8,30 @@ class NewSellController extends AbstractController
 {
     public function index(array $requestData): void
     {
-        $this->render('sells/newSell.php');
+        $error = null;
+        $formData = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $formData = $_POST;
+
+            if (empty($formData['nome']) || empty($formData['cpf_cliente']) || empty($formData['produto']) || empty($formData['status']) || empty($formData['quantidade'])) {
+                $error = '<div class="alert alert-danger" role="alert">Preencha todos os campos!</div>';
+
+            } else {
+                $nome = $formData['nome'];
+                $cpf = $formData['cpf_cliente'];
+                $produto = $formData['produto'];
+                $status = $formData['status'];
+                $quantidade = $formData['quantidade'];
+
+                echo $nome . PHP_EOL . $cpf . PHP_EOL . $produto . PHP_EOL . $status . PHP_EOL . $quantidade;
+            }
+
+        }
+
+        $this->render('sells/newSell.php', [
+            'error' => $error,
+            'formData' => $formData
+        ]);
     }
 }
