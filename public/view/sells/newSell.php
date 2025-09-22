aff6a25f582dfb6ca6a421801d24cf18a8baf6cf
PJJunio <paulodjunior.dev@gmail.com>
2025-09-22 13:37:45 -0300

    feat: construindo as views de sell e newsSell

--- public/view/sells/newSell.php
+++ public/view/sells/newSell.php
@@ -1,7 +1,71 @@
 <?php
 require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../../../app/Database/Query.php';
 
+$query = new \App\Database\Query;
+
+$produtos = $query->select('produto');
 ?>
+
+<main>
+    <div class="container py-5">
+        <div class="mb-4">
+            <h1>Novo produto</h1>
+        </div>
+
+        <div class="w-50 mt-2">
+            <form method="POST" action="/sell/new">
+                <?php if (!empty($data['error'])) {
+                    echo $data['error'];
+                } ?>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome:</label>
+                    <input type="text" class="form-control" id="nome" name="nome"
+                        value="<?php echo htmlspecialchars($data['formData']['nome'] ?? ''); ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="cpf_cliente" class="form-label">CPF do Cliente:</label>
+                    <input type="text" class="form-control" id="cpf_cliente" name="cpf_cliente"
+                        value="<?php echo htmlspecialchars($data['formData']['cpf_cliente'] ?? ''); ?>">
+                </div>
+                <div class="row">
+                    <div class="mb-3 col-4">
+                        <label for="produto" class="form-label">Produto:</label>
+                        <select class="form-select" id="produto" name="produto">
+                            <option selected disabled>Selecione...</option>
+                            <?php
+                            if (is_array($produtos) && count($produtos) > 0) {
+                                foreach ($produtos as $produto) {
+                                    $isSelected = (isset($data['formData']['produto']) && $data['formData']['produto'] == $category['id']) ? 'selected' : '';
+                                    echo "<option value=\"{$produto['nome']}\" {$isSelected}>" . htmlspecialchars($produto['nome']) . "</option>";
+                                }
+                            }
+                            ?>
+
+
+                        </select>
+                    </div>
+                    <div class="mb-3 col-4">
+                        <label for="categoriaId" class="form-label">Status:</label>
+                        <select class="form-select" id="categoriaId" name="categoriaId">
+                            <option selected disabled>Selecione...</option>
+                            <option value="pendente">Pendente</option>
+                            <option value="finalizada">Finalizada</option>
+                            <option value="cancelada">Cancelada</option>
+                        </select>
+                    </div>
+                    <div class="mb-3 col-4">
+                        <label for="quantidade" class="form-label">Quantidade:</label>
+                        <input type="number" class="form-control" id="quantidade" name="quantidade"
+                            value="<?php echo htmlspecialchars($data['formData']['quantidade'] ?? ''); ?>">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </form>
+        </div>
+    </div>
+</main>
 
 <?php
 require_once __DIR__ . '/../includes/footer.php';
--- public/view/sells/sell.php
+++ public/view/sells/sell.php
@@ -1,7 +1,70 @@
 <?php
 require_once __DIR__ . '/../includes/header.php';
+require_once __DIR__ . '/../../../app/Database/Query.php';
 
+$query = new \App\Database\Query;
+
+$vendas = $query->select('venda');
+$itens = $query->select('venda_item');
 ?>
+
+<main>
+    <div class="container py-5">
+        <div class="container py-5">
+            <div class="mb-4 d-flex flex-row justify-content-between">
+                <h1>Produtos</h1>
+                <div>
+                    <a href="sell/new" class="btn btn-primary"><i class="bi bi-plus"></i>Nova venda</a>
+                </div>
+            </div>
+
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Data da venda</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">Qtd.</th>
+                        <th scope="col">Valor</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (is_array($vendas) && count($vendas) > 0) {
+                        foreach ($vendas as $venda) {
+                            if (is_array($itens) && count($itens) > 0) {
+                                foreach ($itens as $item) {
+                                    $valor = $item['preco_unitario'] * $item['quantidade'];
+                                    $produto = $query->select('produto', 'id = :id', [':id' => $item['produto_id']], 'nome');
+                                    ?>
+                                    <tr>
+                                        <th scope="row"><?= $venda['id'] ?></th>
+                                        <td><?= $produto[0]['nome'] ?></td>
+                                        <td><?= date('d-m-Y', strtotime($venda['data_venda'])) ?></td>
+                                        <td><?= $venda['cpf_cliente'] ?></td>
+                                        <td><?= $item['quantidade'] ?></td>
+                                        <td><?= 'R$ ' . number_format($valor, 2, ',', '.') ?></td>
+                                        <td><?= $venda['status'] ?></td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                        }
+                    } else {
+                        ?>
+                        <tr>
+                            <td colspan="7" class="text-center">Nenhum produto encontrado.</td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
 
 <?php
 require_once __DIR__ . '/../includes/footer.php';
